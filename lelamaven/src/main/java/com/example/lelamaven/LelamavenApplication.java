4769536517ccfe57a15e66c7cea5012824252586
REDDY VARI <leela.ujwal@gmail.com>
2022-10-29 10:59:44 +0530

    good to see

---
+++ lelamaven/src/main/java/com/example/lelamaven/LelamavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.lelamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LelamavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LelamavenApplication.class, args);
+	}
+
+}
---
+++ lelamaven/src/test/java/com/example/lelamaven/LelamavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lelamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LelamavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
